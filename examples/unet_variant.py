dfe1327d0ec84513f6cf6f16a9dbcd1ce038cf9b
liammcgoldrick <ljm0015@mix.wvu.edu>
2018-05-22 11:53:23 -0400

    Added comments

--- examples/unet_variant.py
+++ examples/unet_variant.py
@@ -11,10 +11,18 @@ import numpy as np
 f = h5py.File("sample_A_20160501.hdf", "r")
 raw_data=f["volumes"]["raw"].value
 
-affinities=np.load("/home/curie/code/ELEKTRONN2/examples/affinities.npy")
+affinities=np.load("/Users/liammcgoldrick/Code/ELEKTRONN2/examples/affinities.npy")
 
-
+raw_data=np.asarray(raw_data)
 print(np.shape(affinities))
+
+
+
+#data is in format:
+#  raw data=(1,16,128,128) as NP array
+#   affinities=(3,16,128,128) as NP array 
+
+
 
 
                     ###################
@@ -22,20 +30,20 @@ print(np.shape(affinities))
                     ###################
 
 
-in_sh = (None,1,128,128,16)
+in_sh = (None,1,16,128,128)
 
 inp = nm.Input(in_sh, 'b,f,z,x,y', name='raw')  # high res
 
 # Convolution, downsampling of intermediate features
 
 conv0  = nm.Conv(inp,  8,  (15,15,15),(1,1,1),"same",name="c0")
-down0  = nm.Pool(conv0,(4,4,1),mode="max",name="d0")#full
+down0  = nm.Pool(conv0,(1,4,4),mode="max",name="d0")#full
 
 conv1  = nm.Conv(down0,  32,  (15,15,15),(1,1,1),"same",name="c1")
-down1  = nm.Pool(conv1,(2,2,1),mode="max",name="d1")#high
+down1  = nm.Pool(conv1,(1,2,2),mode="max",name="d1")#high
 
 conv2  = nm.Conv(down1,  64,  (15,15,15),(1,1,1),"same",name="c2")#mid
-down2  = nm.Pool(conv2,(2,2,1),mode="max",name="d2")#high
+down2  = nm.Pool(conv2,(1,2,2),mode="max",name="d2")#high
 
 conv3  = nm.Conv(down2,  64,  (15,15,15),(1,1,1),"same",name="c3")#low
 
@@ -52,7 +60,7 @@ mconv1 = nm.Conv(mrg1,8,(15,15,15),(1,1,1),"same",name="mc1")
 mrg2   = nm.UpConvMerge(mconv1, conv0, 8,name="m2",merge_mode="add")
 mconv2 = nm.Conv(mrg2,3,(15,15,15),(1,1,1),"same",name="mc2")
 
-affins=nm.Input((None,3,128,128,16), 'b,f,z,x,y', name='affins')
+affins=nm.Input((None,3,16,128,128), 'b,f,z,x,y', name='affins')
 
 loss_node = nm.loss.BinaryNLL(mconv2,affins)
 loss=nm.AggregateLoss(loss_node)
@@ -78,6 +86,6 @@ model.test_run_prediction()
 
 
 
-model.trainingstep(data= raw_data ,target= affinities ,optimiser="Adam")
+model.trainingstep(data=raw_data ,target= affinities ,optimiser="Adam")
 
 
